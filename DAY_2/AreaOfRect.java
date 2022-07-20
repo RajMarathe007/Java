6e1b47c69a21c72163b7e5630e0af02910565441
Raj Marathe <89590258+RajMarathe007@users.noreply.github.com>
2022-07-20 22:08:07 +0530

    Create AreaOfRect.java

---
+++ DAY_2/AreaOfRect.java
@@ -0,0 +1,16 @@
+public class AreaOfRect
+{
+	public static void main(String[] args)
+	{
+		int length,breadth;
+		length=4;
+		breadth=2;
+		mycode(length,breadth);
+	}
+	
+	private static void mycode(int length, int breadth)
+	{
+		int area = length*breadth;
+		System.out.println("Area of rectangle is : "+area);
+	}
+}
