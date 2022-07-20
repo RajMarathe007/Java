6bec2eb60e2b91e10cb344b60889234fda33bf42
Raj Marathe <89590258+RajMarathe007@users.noreply.github.com>
2022-07-20 21:59:29 +0530

    Create Arithmatic.java

---
+++ DAY_2/Arithmatic.java
@@ -0,0 +1,17 @@
+class Arithmatic
+{
+	public static void main(String args[])
+	{
+		int x = 20;
+		int y = 10;
+		int sum = x+y;
+		int sub = x-y;
+		int mul = x*y;
+		int div = x/y;
+		System.out.println("The Addition of x and Y is " +sum);
+		System.out.println("The Substraction of x and Y is " +sub);
+		System.out.println("The Multiplication of x and Y is " +mul);
+		System.out.println("The Division of x and Y is " +div);
+		
+	}
+}
