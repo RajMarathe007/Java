471c61abebb763f6094c6cf4ab2959cfae98c08f
Raj Marathe <89590258+RajMarathe007@users.noreply.github.com>
2022-07-21 22:14:43 +0530

    Create AreaOfSqu.java

---
+++ DAY_3/AreaOfSqu.java
@@ -0,0 +1,14 @@
+public class AreaOfSqu
+{
+	public static void main (String[] args)
+	{
+		int side = 4;
+		mycode(side);
+	}
+	
+	private static void mycode(int side)
+	{
+		int area = side * side;
+		System.out.println("The area of Square is = "+area);
+	}
+}
