d685c4748a64d7025f3baeef7b088ef94e2836b0
Raj Marathe <89590258+RajMarathe007@users.noreply.github.com>
2022-07-21 22:15:46 +0530

    Create PerimeterOfSqu.java

---
+++ DAY_3/PerimeterOfSqu.java
@@ -0,0 +1,13 @@
+public class PerimeterOfSqu
+{
+	public static void main(String[] args)
+	{
+		int side = 4;
+		mycode(side);
+	}
+	private static void mycode (int side)
+	{
+		int perimeter = 4 * side;
+		System.out.println("Perimeter of a Square is = "+perimeter);
+	}
+}
